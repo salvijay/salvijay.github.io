57284f45eb881c42523fc529c1602078bc1e896b
salvijay <jsalvi449@gmail.com>
2026-05-26 14:35:27 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
+    // Small typing effect
 
+    const text = "Frontend Developer";
+    let i = 0;
+
+    function typing(){
+
+      if(i < text.length){
+
+        document.title = text.substring(0,i+1);
+
+        i++;
+
+        setTimeout(typing,200);
+
+      }
+
+    }
+
+    typing();
